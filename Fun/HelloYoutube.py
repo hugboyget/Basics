6531a6c8cebd4feb6d6bb0410b12e6d5c86a8aa8
JustF1y <jjf@nyist.edu.cn>
2021-07-02 14:50:04 +0800

    GUI-3-button

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -6,25 +6,33 @@
 
 from tkinter import *
 
+# button = you click it, then it does stuff
+
+count = 0
+
+def click():
+    global count
+    count += 1
+    print(count)
+    print("You clicked the button !")
+
 window = Tk()
 
-photo = PhotoImage(file='lo.gif')
+photo = PhotoImage(file='icon_love.png')
 
 
-#label = an area widget that holds text and/or an image within a window
-
-label = Label(window,
-              text="Hello World !",
-              font=('Arial', 40, 'bold'),
-              fg='#00FF00',
-              background='black',
-              relief=RAISED,
-              bd=10,
-              padx=20,
-              pady=20,
-              image=photo,
-              compound='top')
-label.pack()
-#label.place(x=100, y=100)
+button = Button(window,
+                text='click me !',
+                command=click,
+                font=("Comic Sans", 30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                #state=DISABLED,
+                image=photo,
+                compound='top')
+button.pack()
 
 window.mainloop()
+
