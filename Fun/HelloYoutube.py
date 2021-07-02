5ac164d738c8c0f07cd079aa8a797f40da63a994
JustF1y <jjf@nyist.edu.cn>
2021-07-02 14:01:48 +0800

    GUI-1

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -4,5 +4,16 @@
 #@File : HelloYoutube.py
 #@Software: PyCharm
 
-what is faker hsadfjkasfjksd
+from tkinter import *
+
+window = Tk() #instantiate an instance of a window
+
+window.geometry("490x490")
+window.title("Bro Code first GUI program")
+
+icon = PhotoImage(file="lo.gif")# PhotoImage图片必须gif格式
+window.iconphoto(True, icon)
+window.config(background="black")
+
+window.mainloop() #place window on computer screen. listen for events
 
