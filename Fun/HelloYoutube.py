485265b075781e1745959d7edf66d45bca3df6a2
JustF1y <jjf@nyist.edu.cn>
2021-07-04 13:10:46 +0800

    GUI-9-listbox

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -4,58 +4,55 @@
 #@File : HelloYoutube.py
 #@Software: PyCharm
 
-# listbox = A list of selectabel text items within it's own container
+
 from tkinter import *
+from tkinter import messagebox
 
-def submit():
-    print("You have ordered:")
-    for i in listbox.curselection():
-        print(listbox.get(i))
 
-def add():
-    listbox.insert(listbox.size(), entryBox.get())
-    listbox.config(height=listbox.size())
+def click():
+    #messagebox.showinfo(title='INFO!', message='You are aperson.')
+    #messagebox.showwarning(title='WARNING!', message='You have A VIRUS !')
+    #messagebox.showwarning(title='ERROR!', message='something went wrong :(')
 
-def delete():
-    for i in reversed(listbox.curselection()):
-        listbox.delete(i)
-        listbox.config(height=listbox.size())
+    #if messagebox.askokcancel(title='ask ok cancel', message='Do it ?'):
+    #    print("You do it.")
+    #else:
+    #    print("You canceled it.")
+
+    #if messagebox.askretrycancel(title='ask retry cancel', message='重试 ?'):
+    #    print("You retry it.")
+    #else:
+    #    print("You canceled it.")
+
+    #if messagebox.askyesno(title='ask yes or no', message='Do you like cake ?'):
+    #   print('I like cake too:)')
+    #else:
+    #   print('Why do you not like cake:(')
+
+    # answer = messagebox.askquestion(title='ask question', message='Do you like pie!')
+    # if(answer == 'yes'):
+    #     print('me too')
+    # else:
+    #     print('why ?')
+
+    answer = messagebox.askyesnocancel(title='Yes no cancel',
+                                       message='Do you like to code?',
+                                       icon='warning')
+    #icon= info,warning,error
+    if answer == 'True':
+        print("you like")
+    elif answer == False:
+        print("you dot like")
+    else:
+        print("you dodged it.")
+
+
 
 
 window = Tk()
 
-listbox = Listbox(window,
-                  bg="pink",
-                  font=("Constantia", 15),
-                  width=12,
-                  selectmode=MULTIPLE)
-listbox.pack()
-
-listbox.insert(1, "pizza")
-listbox.insert(2, "pasta")
-listbox.insert(3, "garlic bread")
-listbox.insert(4, "soup")
-listbox.insert(5, "salad")
-
-listbox.config(height=listbox.size())
-
-
-entryBox = Entry(window,
-                 bg='green')
-entryBox.pack()
-
-
-submitButton = Button(window, text="submit", command=submit)
-submitButton.pack()
-
-addButton = Button(window, text="add", command=add)
-addButton.pack()
-
-deleteButton = Button(window, text="delete", command=delete)
-deleteButton.pack()
-
-
-
+button = Button(window, command=click, text='click me!!!')
+button.pack()
 
 window.mainloop()
 
---
+++ Fun/test.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+#@Time : 2021/7/4 12:27
+#@Author : HUGBOY
+#@File : test.py
+#@Software: PyCharm
+
+for i in reversed(100):
+    print(i)
