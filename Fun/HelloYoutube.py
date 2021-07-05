584d2df31ce252c430f2ee0a11cccc230bb88d26
JustF1y <jjf@nyist.edu.cn>
2021-07-05 23:09:08 +0800

    GUI-10-colorchooser

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -4,56 +4,17 @@
 #@File : HelloYoutube.py
 #@Software: PyCharm
 
-
-
 from tkinter import *
-from tkinter import messagebox
-
+from tkinter import colorchooser
 
 def click():
-    #messagebox.showinfo(title='INFO!', message='You are aperson.')
-    #messagebox.showwarning(title='WARNING!', message='You have A VIRUS !')
-    #messagebox.showwarning(title='ERROR!', message='something went wrong :(')
-
-    #if messagebox.askokcancel(title='ask ok cancel', message='Do it ?'):
-    #    print("You do it.")
-    #else:
-    #    print("You canceled it.")
-
-    #if messagebox.askretrycancel(title='ask retry cancel', message='重试 ?'):
-    #    print("You retry it.")
-    #else:
-    #    print("You canceled it.")
-
-    #if messagebox.askyesno(title='ask yes or no', message='Do you like cake ?'):
-    #   print('I like cake too:)')
-    #else:
-    #   print('Why do you not like cake:(')
-
-    # answer = messagebox.askquestion(title='ask question', message='Do you like pie!')
-    # if(answer == 'yes'):
-    #     print('me too')
-    # else:
-    #     print('why ?')
-
-    answer = messagebox.askyesnocancel(title='Yes no cancel',
-                                       message='Do you like to code?',
-                                       icon='warning')
-    #icon= info,warning,error
-    if answer == 'True':
-        print("you like")
-    elif answer == False:
-        print("you dot like")
-    else:
-        print("you dodged it.")
-
-
-
+    color = colorchooser.askcolor()
+    print(color)
+    colorHex = (color[1])
+    window.config(bg=colorHex)#change background
 
 window = Tk()
-
-button = Button(window, command=click, text='click me!!!')
+window.geometry("420x420")
+button = Button(text='click me', command=click)
 button.pack()
-
 window.mainloop()
-
