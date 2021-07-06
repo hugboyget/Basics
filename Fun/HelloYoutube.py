3d18c42a36f88983b8f3ff02e07bcfa7e63fa60a
JustF1y <jjf@nyist.edu.cn>
2021-07-07 00:20:15 +0800

    GUI-15-frame

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -4,41 +4,21 @@
 #@File : HelloYoutube.py
 #@Software: PyCharm
 
+# frame = a rectangular container to group and hold widgets
+
 from tkinter import *
 
-def openFile():
-    print("File has been opened!")
-def saveFile():
-    print("File has been saved!")
-def cut():
-    print("You cut some text !")
-def copy():
-    print("You copy some text !")
-def paste():
-    print("You paste some text !")
 window = Tk()
 
-openImage = PhotoImage(file="open.png")
-saveImage = PhotoImage(file="save.png")
-exitImage = PhotoImage(file="exit.png")
+frame = Frame(window, bg="black", bd=5, relief=SUNKEN)
+frame.place(x=60, y=60)
 
-menubar = Menu(window)
-window.config(menu=menubar)
+Button(frame, text="W", font=("Consolas", 25), bg='light green').pack(side=TOP)
+Button(frame, text="A", font=("Consolas", 25), bg='light green').pack(side=LEFT)
+Button(frame, text="S", font=("Consolas", 25), bg='light green').pack(side=LEFT)
+Button(frame, text="D", font=("Consolas", 25), bg='light green').pack(side=LEFT)
 
-fileMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
-menubar.add_cascade(label="File", menu=fileMenu)
 
-fileMenu.add_command(label="Open", command=openFile, image=openImage, compound="left")
-fileMenu.add_command(label="Save", command=saveFile, image=saveImage, compound="left")
-fileMenu.add_separator()#分隔线
-fileMenu.add_command(label="Exit", command=quit, image=exitImage, compound="left")
-
-editMenu = Menu(menubar, tearoff=0, font=("MV Boli", 15))
-menubar.add_cascade(label="Edit", menu=editMenu)
-
-editMenu.add_command(label="Cut", command=cut)
-editMenu.add_command(label="Copy", command=copy)
-editMenu.add_command(label="Paste", command=paste)
 
 
 window.mainloop()
