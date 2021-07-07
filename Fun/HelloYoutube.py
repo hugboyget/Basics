a51ef2b100bd94ed10059fadd2d5669780971d8f
JustF1y <jjf@nyist.edu.cn>
2021-07-07 17:57:11 +0800

    GUI-20-canvas

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -5,36 +5,21 @@
 #@Software: PyCharm
 
 from tkinter import *
-from tkinter.ttk import *
-import time
-
-
-def start():
-    GB = 10
-    download = 0
-    speed = 1
-    while(download<GB):
-        time.sleep(0.05)
-        bar['value'] += 10
-        download+=speed
-        percent.set(str((download/GB)*100)+"%")
-        text.set(str(download)+'/'+str(GB)+" GB completed")
-        window.update_idletasks()
-
-
 
 window = Tk()
 
-percent = StringVar()
-text = StringVar()
-
-bar = Progressbar(window, orient=HORIZONTAL, length=300)
-bar.pack(pady=10)
-
-
-percentLabel = Label(window, textvariabl=percent).pack()
-taskLabel = Label(window, textvariable=text).pack()
-button = Button(window, text="download", command=start).pack()
+canvas = Canvas(window, height=500, width=500)
+canvas.create_line(0, 0, 500, 500, fill="blue", width="5")
+canvas.create_line(0, 500, 500, 0, fill="red", width="10")
+canvas.create_rectangle(20, 20, 250, 250, fill="pink")
+canvas.create_polygon(250, 0, 500, 500, 0, 500, fill="green", outline="black", width=5)
+points = [250, 0, 500, 500, 0, 500]
+canvas.create_polygon(points, fill="yellow", outline="black", width=5)
+canvas.create_arc(0, 0, 200, 200, fill="green", style=PIESLICE, start=45, extent=180)
+canvas.create_arc(0, 0, 500, 500, fill="red", extent=180, start=0, width=10)
+canvas.create_arc(0, 0, 500, 500, fill="white", extent=180, start=180, width=10)
+canvas.create_oval(190, 190, 310, 310, fill="white", width=10)
+canvas.pack()
 
 window.mainloop()
 
