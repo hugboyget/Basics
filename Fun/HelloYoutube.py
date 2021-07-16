fa09db45aab02fa5b2a40159e585a86e0253ebd8
JustF1y <jjf@nyist.edu.cn>
2021-07-16 11:06:45 +0800

    GUI-21-keyboard_events

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -6,21 +6,22 @@
 
 from tkinter import *
 
+def doSomething(event):
+    #print("dit a thing")
+    print("Hello. !")
+    print("You did a thing:", str(event.x)+","+str(event.y))
+    label.config(text=event.keysym)
+
 window = Tk()
 
-canvas = Canvas(window, height=500, width=500)
-canvas.create_line(0, 0, 500, 500, fill="blue", width="5")
-canvas.create_line(0, 500, 500, 0, fill="red", width="10")
-canvas.create_rectangle(20, 20, 250, 250, fill="pink")
-canvas.create_polygon(250, 0, 500, 500, 0, 500, fill="green", outline="black", width=5)
-points = [250, 0, 500, 500, 0, 500]
-canvas.create_polygon(points, fill="yellow", outline="black", width=5)
-canvas.create_arc(0, 0, 200, 200, fill="green", style=PIESLICE, start=45, extent=180)
-canvas.create_arc(0, 0, 500, 500, fill="red", extent=180, start=0, width=10)
-canvas.create_arc(0, 0, 500, 500, fill="white", extent=180, start=180, width=10)
-canvas.create_oval(190, 190, 310, 310, fill="white", width=10)
-canvas.pack()
+#window.bind("<Key>", doSomething)
+window.bind("<Button-1>", doSomething)#mouse left click
+window.bind("<Button-2>", doSomething)#scroll wheel
+window.bind("<Button-3>", doSomething)#right mouse click
+window.bind("<Leave>", doSomething)#leave the window
+window.bind("<Enter>", doSomething)#Enter the window
+window.bind("<Motion>", doSomething)#where the mouse moved
+label = Label(window, font=("Helvetica", 100))
+label.pack()
 
 window.mainloop()
-
-
