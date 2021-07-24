c31f9fdd4ba58aa338e207025ac18b0c6a856f6f
JustF1y <jjf@nyist.edu.cn>
2021-07-24 20:44:41 +0800

    GUI-26-Ball_animations

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -5,37 +5,28 @@
 #@Software: PyCharm
 
 from tkinter import *
+from Ball import *
 import time
-
-WIDTH = 600
-HEIGHT = 400
-
-xVelocity =1
-yVelocity =2
 
 window = Tk()
 
+WIDTH = 600
+HEIGHT = 600
+
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
+window.update()
 canvas.pack()
-background_photo = PhotoImage(file='city.png')
-background = canvas.create_image(0, 0, image=background_photo)
 
-photo_image = PhotoImage(file='airship.png')
-my_image = canvas.create_image(0, 0, image=photo_image, anchor=NW)
-
-image_width = photo_image.width()
-image_height = photo_image.height()
+volley_ball = Ball(canvas, 0, 0, 100, 1, 1, "red")
+tennis_ball = Ball(canvas, 0, 0, 50, 4, 3, "yellow")
+basket_ball = Ball(canvas, 0, 0, 125, 8, 7, "orange")
 
 while True:
-    coordinates = canvas.coords(my_image)
-    print("飞船当前坐标:"+str(coordinates))
-    if coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0:
-        xVelocity = -xVelocity
-    if coordinates[1]>=(HEIGHT-image_height) or coordinates[1]<0:
-        yVelocity = -yVelocity
 
-    canvas.move(my_image, xVelocity, yVelocity)
     window.update()
+    volley_ball.move()
+    tennis_ball.move()
+    basket_ball.move()
     time.sleep(0.01)
 
 window.mainloop()
