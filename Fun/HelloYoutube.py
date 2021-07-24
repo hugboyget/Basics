532365476e418c803cc6475761f18cc9da5c6771
JustF1y <jjf@nyist.edu.cn>
2021-07-24 16:06:52 +0800

    GUI-22-drag&drop

--- Fun/HelloYoutube.py
+++ Fun/HelloYoutube.py
@@ -6,18 +6,12 @@
 
 from tkinter import *
 
-def doSomething(event):
-    print("You did a thing:" + str(event.x) + "," + str(event.y))
+
 
 window = Tk()
 
-window.bind("<Button-1>", doSomething) #left mouse click
-#window.bind("<Button-2>", doSomething) #scroll whell
-#window.bind("<Button-3>", doSomething) #right mouse click
-#window.bind("<ButtonRealease>", doSomething)
-window.bind("<Enter>", doSomething) #enter the window
-window.bind("<Leave>", doSomething) #leave the window
-window.bind("<Motion>", doSomething) #Where the mouse moved
+
+
 
 
 
